6a4fbaadeaf64a9c8f53bcc8bda90b00e71cdf5b
kotofey97 <olya_sitnikova@mail.ru>
2021-10-06 14:38:31 +0300

    v.1

---
+++ kittybot.py
@@ -0,0 +1,59 @@
+import logging
+import os
+
+import requests
+from dotenv import load_dotenv
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+load_dotenv()
+secret_token = os.getenv('TOKEN')
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+def new_cat(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        reply_markup=button)
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def main():
+    updater = Updater(token=secret_token)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling(poll_interval=20.0)
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
